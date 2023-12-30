3f89c96c484aae299bae6335b4abb269320dcc5c
Jesús Martínez <jesus.martinez.gonzalez1993@gmail.com>
2023-12-30 11:07:12 +0100

    añadiendo html del componente desde la configuración

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-empleado',
   templateUrl: './empleado.component.html',
+  //template:"<p>Aqui iria un empleado<p>" aqui metemos el html y podemos precindir de un fichero a parte de html
   styleUrls: ['./empleado.component.css'],
   //styles:["p{background-color:red;}"] esto es cuando queremos prescindir de usar un fichero css para los estilos,(Aqui lo añadimos directamente)
 })
