561d49db0e711540472a4761668e2bca91381d1e
Jesús Martínez <jesus.martinez.gonzalez1993@gmail.com>
2023-12-30 10:43:10 +0100

    creación y registro de un componente nuevo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+// SELECTOR ES EL NOMBRE QUE SE USARA PARA LLAMAR ESTE APP EN EL HTML INDEX PRINCIPAL SE LLAMARA ASI >> <app-root></app-root>
+// templateUrl  ES LA URL DEL INDEX DE LA APP
+//styleUrls ES LA URL DE LA HOJA DE ESTILOS DE LA APP
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+//PROPIEDADES BASICAS DE ESTA APP EN HTML DE LA APP DE LLAMAN ASI {{title}}
 export class AppComponent {
   title = 'miprimeraApp';
 }
